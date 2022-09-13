f841365e720067efaa59c0c1d22b73ff6d5ea00c
gastongar <112703340+gastongar@users.noreply.github.com>
2022-09-12 23:45:28 -0300

    sube

---
+++ Dia 7/clases.py
@@ -0,0 +1,20 @@
+class Pajaro:
+
+    alas = True
+    def __init__(self, color,especie):
+        self.color = color
+        self.especie = especie
+
+
+
+
+mi_pajaro = Pajaro('negro','Tucan')
+otro_pajaro = Pajaro('blanco','paloma')
+palabra = 'hola'
+
+print(mi_pajaro.color)
+print(otro_pajaro.alas)
+
+
+#print(mi_pajaro)
+#print(otro_pajaro)
