7b19495d7670fa141979a70632d13c6862dee0ec
gastongar <112703340+gastongar@users.noreply.github.com>
2022-09-13 23:31:38 -0300

    sube

--- Dia 6/path practica.py
+++ Dia 6/path practica.py
@@ -10,3 +10,15 @@ print(guia2.parent)
 
 
 
+from pathlib import Path
+
+base = Path.home()
+guia = Path(base,"Europa","Espania", Path("Barcelona", "Sagrada_Familia.txt"))
+print(base)
+print(guia)
+
+guia2= guia.with_name("La_Pedrera.txt")
+print(guia2.parent)
+
+
+
