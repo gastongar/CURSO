d8297c06e64bbe22be5921a61c475c220895ec7d
gastongar <112703340+gastongar@users.noreply.github.com>
2022-09-16 21:06:40 -0300

    sube

---
+++ DESCARGAS/practica_pylint.py
@@ -0,0 +1,20 @@
+"""
+Esta es un a practica
+"""
+
+
+def sumar(numero1, numero2):
+
+    """
+    sumar numeros
+    """
+
+    return numero1 + numero2
+
+
+SUMA = sumar(5, 7)
+
+
+print(f"el resultado de la suma es {SUMA}")
+
+
