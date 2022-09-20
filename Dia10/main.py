57c07623c2e06947de9390464f2b09e9c924c734
gastongar <garabellogaston@gmail.com>
2022-09-20 16:23:21 -0300

    sube

---
+++ Dia10/main.py
@@ -0,0 +1,6 @@
+import pygame
+
+pygame.init()
+
+pantalla = pygame.display.set.mode((800, 600))
+
---
+++ mipaquete/ejercicio.py
@@ -0,0 +1,6 @@
+from paquete_gaston import suma_y_resta
+from paquete_gaston.sub_paquete import saludo
+suma_y_resta.resta(15,2)
+suma_y_resta.suma(20,55)
+
+saludo.hola()
